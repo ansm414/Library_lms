78763591def3b1ca45fd0b6b05b5ab95dbd19990
Muhammad Ans <m.ans.eritheialabs@gmail.com>
2022-05-13 15:05:17 +0500

    added query in seeds file and new form of user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Role.create(name:"Admin") 
+Role.create(name:"Admin") 
 Role.create(name:"Reader") 
 
 
 # User.create(email:"bsem-f18-115@superior.edu.pk", role_id: 1, name:"muhammad ans",address: "iqbal town",gender: "male")
-# User.create(email:"bsem-f18-115@superior.edu.pk", role_id: 8, name:"muhammad ans",address: "iqbal town",gender: "male",password: 12345678,password_confirmation:12345678)
+User.create(email:"bsem-f18-115@superior.edu.pk", role_id: Role.where(name:"Admin").first.id,
+ name:"muhammad ans",address: "iqbal town",gender: "male",password: 12345678,password_confirmation:12345678)
