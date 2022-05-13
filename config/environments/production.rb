1a3cc66dcd38504d3747f5471537f7d2d11246c3
Muhammad Ans <m.ans.eritheialabs@gmail.com>
2022-05-13 11:32:57 +0500

    add credentails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.hosts << /[a-z0-9]+\.c9users\.io/
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -87,6 +88,22 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.active_record.dump_schema_after_migration = false
+  config.action_controller.asset_host = ENV['DOMAIN']
+  config.action_mailer.asset_host = ENV['DOMAIN']
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp-relay.sendinblue.com',
+    :port           => '587',
+    :authentication => :login,
+    :user_name      => ENV['SENDINBLUE_USERNAME'],
+    :password       => ENV['SENDINBLUE_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
